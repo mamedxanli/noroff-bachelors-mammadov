201fb8c43f71ff058e7277e9dc74d485b4ac7071
Alek Mammadov <mamedxanli@gmail.com>
2020-01-01 20:36:34 +0100

    improving parser

--- auth_log_archive/ip_and_date_parser.py
+++ auth_log_archive/ip_and_date_parser.py
@@ -1,12 +1,12 @@
 import re
 import sys
 
-iplist = []
+iplist = {}
 logfile = sys.argv[1]
 
 with open(logfile, 'r') as f:
     for line in f:
-        ip = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line)
-        if ip not in iplist:
-            iplist.append(ip)
+        ip = (re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line))
+        timestamp = line[0:15]
+        iplist.update({str(timestamp):str(ip)[2:-2]})
     print(iplist)
