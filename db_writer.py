35aae4058e5d8067b86c3823147f9f3638cdbecd
Alek Mammadov <mamedxanli@gmail.com>
2020-01-17 14:39:42 +0100

    parser is ready

--- db_writer.py
+++ db_writer.py
@@ -14,20 +14,23 @@ measurement = 'ssh_honeypot'
 
 
 def get_geolocation(ip):
-    data = geoip.geolite2.lookup(ip)
-    return data.to_dict()
-
+        data = geoip.geolite2.lookup(ip)
+        return data.to_dict()
 
 def process_input_data(logfile):
-    regex_value = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-    #reads logfile, parses it and returns epoch, ip, continent, country, location and geohash
+    regex_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    regex_value = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+    all_lines_processed = list()
     with open(logfile, 'r') as f:
-        for line in f:
-            #with suppress(Exception):
-                try:
-                    ip = str((re.findall(regex_value, line)))
+        #while True:
+            for line in f:
+                if re.findall(regex_pattern, line) == []:
+                    continue
+                else:
+                    output = {}
+                    ip = str(re.findall(regex_pattern, line))[2:-2]
+                    geodata = get_geolocation(ip)
                     epoch = line[0:26]
-                    geodata = get_geolocation(ip[2:-2])
                     continent = geodata['continent']
                     country = geodata['country']
                     location = geodata['location']
@@ -35,9 +38,16 @@ def process_input_data(logfile):
                     latitude = latlong[0]
                     longitude = latlong[1]
                     geohash_data = geohash.encode(float(latitude[1:]), float(longitude[:0-1]))
-                    #print(epoch, ip, continent, country, location, geohash_data)
-                    return epoch, ip, continent, country, location, geohash_data
-                except Exception as exc:
-                    raise ValueError(f"Exception while getting geodata: {exc}.")
+                    output["ip"] = ip
+                    output["epoch"] = epoch
+                    output["continent"] = continent
+                    output["country"] = country
+                    output["latitude"] = latitude[1:]
+                    output["longitude"] = longitude[:-1]
+                    output["geohash_data"] = geohash_data
+                all_lines_processed.append(output)
+            return all_lines_processed
+
 data = process_input_data(logfile)
 print(data)
+
