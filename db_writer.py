52de918e9304f9f24636e437c22b09914e9f166b
Alek Mammadov <mammadov.alek@linux.com>
2020-01-17 16:16:02 +0100

    adding influx struture

--- db_writer.py
+++ db_writer.py
@@ -48,6 +48,48 @@ def process_input_data(logfile):
                 all_lines_processed.append(output)
             return all_lines_processed
 
-data = process_input_data(logfile)
-print(data)
+def build_influx_tcp_structure(measurement: str, epoch: str, tags: dict = {}, fields: dict = {}) -> dict:
+    """
+    Builds data structure for Influx. To be used in the TCP port localhost:8086.\n
+    This port is the InfluxDB Docker container running.
+    If no epoch is passed, defaults to datetime.now().\n
+    Expects:\n
+        - measurement: Main value to search in Grafana. (similar to Table name)
+        - value: The value of said measurement. Defaults to float 1. Unless the measurement is really simple with no relation to "fields", leave it at default.
+        - epoch: Timestamp of the measurement. Format is 2019-03-20T00:08:44
+        - fields: Optional: Dict if more fields want to be stored. Fields can contain values, like cpu usage, temperature, or traffic. Usually used with numeric values for plot.
+        - tags: Optional: Dict with tags for this measurement. Tags can be used as identifiers or labels. Usually strings to sort data by groups.
+    """
+    try:
+        
+        # value goes into fields, more like a placeholder
+        fields["value"] = float(value)
+        # Main dictionary
+        data = {
+            "measurement" : measurement,
+            "epoch"       : epoch
+            }
+        # Nest dictionaries
+        data["tags"] = tags
+        tags = {
+            "country"
+            "continent"
+        }
+        data["fields"] = fields
+        fields = {
+            "epoch" : epoch,
+            "ip" : ip,
+            "geohash_data" : geohash_data,
+            "latitude" : latitude,
+            "latitude" : latitude,
+        }
+        print(data) # Debug
+        return data
+    except Exception as exc:
+        raise ValueError(f"Exception while building influx tcp structure: {exc}.")
+
+input_data = process_input_data(logfile)
+build_influx_tcp_structure('ssh_honeypot', input_data)
+
+#print(data)
 
