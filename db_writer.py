e7c69ca93d6bc5baff1a664ab1082b9bd806de30
Alek Mammadov <mamedxanli@gmail.com>
2020-01-18 23:12:08 +0100

    removing needless lines

--- db_writer.py
+++ db_writer.py
@@ -12,6 +12,7 @@ logfile = '/var/log/auth.log'
 dbname = 'honeypot'
 measurement = 'ssh_honeypot'
 
+
 def get_geolocation(ip):
         try:
             data = geoip.geolite2.lookup(ip)
@@ -21,9 +22,9 @@ def get_geolocation(ip):
         except Exception as exc:
             raise ValueError(f"Exception while building influx tcp structure: {exc}.")
 
+
 def parser(logfile):
     regex_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-    #regex_value = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
     all_lines_processed = list()
     with open(logfile, 'r') as f:
         for line in f:
@@ -52,6 +53,8 @@ def parser(logfile):
                     ready_influx_structure = build_influx_tcp_structure(epoch=epoch, tags=tags_dict, fields=fields_dict)
                     all_lines_processed.append(ready_influx_structure)
         return all_lines_processed
+
+
 def build_influx_tcp_structure(epoch: str, measurement: str = "ssh_honeypot", value: float = 1.0, tags: dict = {}, fields: dict = {}) -> dict:
     """
     Builds data structure for Influx. To be used in the TCP port localhost:8086.\n
@@ -81,18 +84,14 @@ def build_influx_tcp_structure(epoch: str, measurement: str = "ssh_honeypot", va
         raise ValueError(f"Exception while building influx tcp structure: {exc}.")
 
 
-
 def write_to_db(data):
     print("checking if writer gets data: ", data)
     db_connection = influxdb.InfluxDBClient('influx', '8086', 'admin', 'admin123', dbname)
     try:
         db_connection.write_points(data)
-        #continue
     except Exception as exc:
         raise InfluxDBClientError(f"Exception while writing to db: {exc}.")
 
 
-#if __name__ ==' __main__':
 influx_data = parser(logfile)
 write_to_db(influx_data)
-
